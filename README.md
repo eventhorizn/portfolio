b680cb492f37f5f763d94210128e14c2b0f4dc2d
Gary Hake <Gary.Hake@waystar.com>
2021-02-16 12:27:25 -0500

    technical skills section added links to github and linked in starting on stats section

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
    - [Superslides](https://github.com/nicinabox/superslides)
 1. For pictures
    - [Pexels](https://www.pexels.com/)
+1. For site color scheme
+   - [Flat UI Colors](https://flatuicolors.com/)
+1. For skills carousel
+   - Owl Carosel (https://owlcarousel2.github.io/OwlCarousel2/)
+1. For skills pie charts
+   - Easy pie chart (https://github.com/rendro/easy-pie-chart)
--- js/script.js
+++ js/script.js
@@ -6,6 +6,45 @@ jQuery(function () {
 		pagination: false,
 	});
 
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		items: 4,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			480: {
+				items: 2,
+			},
+			768: {
+				items: 3,
+			},
+			938: {
+				items: 4,
+			},
+		},
+	});
+
+	const skillsTopOffset = $('.skillsSection').offset().top;
+
+	$(window).on('wheel', function () {
+		if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+			$(function () {
+				$('.chart').easyPieChart({
+					easing: 'easeInOut',
+					barColor: '#fff',
+					trackColor: false,
+					scaleColor: false,
+					lineWidth: 4,
+					size: 152,
+					onStep: function (from, to, percent) {
+						$(this.el).find('.percent').text(Math.round(percent));
+					},
+				});
+			});
+		}
+	});
+
 	// const typed = new Typed('.typed', {
 	// 	strings: ['Software Engineer', 'Web Developer'],
 	// 	typeSpeed: 70,
